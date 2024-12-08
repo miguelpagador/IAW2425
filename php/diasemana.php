2099adb97b7af2d786aba7a165dedb4992cbc2e0
Miguel <miguelpagador04@iesamachado.org>
2024-12-08 17:52:59 +0100

    cambios

--- php/diasemana.php
+++ php/diasemana.php
@@ -13,7 +13,7 @@ $dia = date('N');
         if (case = 1){
         echo "<p>Hoy en lunes, comienza la semana.</p>";}
         else if (case = 2){
-        echo "<p>Orale putos, ya es martes. ¡¡¡¡¡AAAAAAAAAAAAAAAAAAAAAAAAA!!!!!</p>";}
+        echo "<p>Ya es martes.</p>";}
         else if (case = 3){
         echo "<p>Miércoles, siempre en medio.</p>";}
         else if (case = 4){
@@ -21,11 +21,11 @@ $dia = date('N');
         else if (case = 5){
         echo "<p>Viernes, un poco más y a descansar.</p>";}
         else if (case = 6){
-        echo "<p>Sábado, sabadete. La rima se hace sola.</p>";}
+        echo "<p>Sábado, sabadete.</p>";}
         else if (case = 7){
-        echo "<p>Domingo... espero que esté todo listo para el lunes</p>";}
+        echo "<p>Domingo... </p>";}
         else {
-        echo "<p>Esto sale si no carga los días por alguna razón. ¿Qué has roto, Jesús?</p>";}
+        echo "<p>Esto sale si no carga los días por alguna razón.</p>";}
 }
 ?>
 </body>
