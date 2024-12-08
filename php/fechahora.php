66b6fca640d3a5f0420891a68b227028d1ef64e4
Miguel <miguelpagador04@iesamachado.org>
2024-12-08 17:52:32 +0100

    cambios

--- php/fechahora.php
+++ php/fechahora.php
@@ -8,10 +8,10 @@
 </head>
 <body>
     <h1>Fecha y hora actual</h1>
-<?php
-setlocale(LC_TIME, 'es_ES.UTF-8');
-$hechayhora = strftime('%A, %d de %B del año %Y y la hora es: %H:%M:%S');
-echo "<p>$hechayhora</p>";
-?>
+    <?php
+        date_default_timezone_set('Europe/Madrid');
+        setlocale(LC_TIME, 'es_ES.UTF-8', 'spanish');
+        echo "Hoy es " . date("l") . " " . date("d/m/Y") . " y son las " . date("h:i:sa");
+    ?>
 </body>
 </html>
