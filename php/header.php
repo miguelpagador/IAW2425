ba97bdf27bc1c62b518bfbad3985f77ad7f51078
Miguel Pagador Perez <miguelpagador04@iesamachado.org>
2025-01-13 14:07:49 +0100

    cambios

---
+++ php/header.php
@@ -0,0 +1,16 @@
+<?php
+header("refresh:3;url=https://sevillafc.es/");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+<?php
+echo "Te estamos redirigiendo a nuestra página. Por favor, espera...";
+?>
+</body>
+</html>
